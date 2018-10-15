9a861b7858ebc25c99139d325228091aa22f604f
Siddharth, Bhardwaj <bhrsiddharth94@gmail.com>
2018-10-15 20:05:40 +0530

    updted status
    updated

--- complete-javascript-course-master/7-ES6/starter/script.js
+++ complete-javascript-course-master/7-ES6/starter/script.js
@@ -18,6 +18,7 @@ console.log(name6);
 
 function driversLicence(passedTest) {
     if (passedTest) {
+        console.log(firstName);
         var firstName = 'John';
         var yearOfBirth = 1990;
     }        
@@ -30,12 +31,99 @@ driversLicence(true);
 //ES6
 
 function driversLicence6(passedTest) {
+    //console.log(firstName);
+    let firstName;
+    const yearOfBirth = 1990;
     if (passedTest) {
-        let firstName = 'John';
-        const yearOfBirth = 1990;   
+        firstName = 'John';   
     }
         console.log(firstName + ', born in ' + yearOfBirth + ' is now officialyy alowed to drive a car.');
 }
 
 
 driversLicence6(true);
+
+
+
+
+
+
+
+
+
+
+// Block Scoping Example
+
+let i = 23;
+
+for(let i = 0; i < 5; i++) {
+    console.log(i);
+}
+
+console.log(i);
+
+
+
+////////////////////////////////////
+// Lecture: Blocks and IIFs
+
+{
+    const a = 1;
+    let b = 2;
+    //var c =3;
+}
+
+//console.log(a + b);
+//console.log(c);
+
+
+
+//ES5
+
+(function() {
+    var c = 3;
+})();
+
+//console.log(c);
+
+
+
+//////////////////////////////
+// Lecture: Strings
+
+let firstName = 'John';
+let lastName = 'Smith';
+const yearOfBirth = 1990;
+
+function calcAge(year) {
+    return 2016 - year;
+}
+
+// ES5
+
+console.log('This is ' + firstName + ' ' + lastName + '. He was born in ' + yearOfBirth + '. Today, he is '+ calcAge(yearOfBirth) + ' years old.');
+
+// ES6
+
+console.log(`This is ${firstName} ${lastName}. He was born in ${yearOfBirth}. Today, he is ${calcAge(yearOfBirth)} years old.`);
+
+const n = `${firstName} ${lastName}`;
+
+//ES6 String functions
+console.log(n.startsWith('J'));
+console.log(n.endsWith('h'));
+console.log(n.includes(' '));
+console.log(firstName.repeat(5));
+console.log(`${lastName}`.repeat(5));
+
+
+
+
+///////////////////////////////////
+// Lecture: Arrow Functions
+
+const years = [1990, 1965, 1982, 1937];
+
+// ES5
+var 
+

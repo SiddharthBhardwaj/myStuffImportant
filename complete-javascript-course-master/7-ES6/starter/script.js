473ec6905d071754c5c0822c4a117a48b30e21a2
Siddharth, Bhardwaj <bhrsiddharth94@gmail.com>
2018-10-16 17:30:34 +0530

    progress done
    progress done

--- complete-javascript-course-master/7-ES6/starter/script.js
+++ complete-javascript-course-master/7-ES6/starter/script.js
@@ -16,35 +16,32 @@ console.log(name6);
 */
 //ES5
 
-function driversLicence(passedTest) {
-    if (passedTest) {
-        console.log(firstName);
-        var firstName = 'John';
-        var yearOfBirth = 1990;
-    }        
-        console.log(firstName + ', born in ' + yearOfBirth + ' is now officialyy alowed to drive a car.');
-}
+// function driversLicence(passedTest) {
+//     if (passedTest) {
+//         console.log(firstName);
+//         var firstName = 'John';
+//         var yearOfBirth = 1990;
+//     }        
+//         console.log(firstName + ', born in ' + yearOfBirth + ' is now officialyy alowed to drive a car.');
+// }
 
 
-driversLicence(true);
+// driversLicence(true);
 
-//ES6
+// //ES6
 
-function driversLicence6(passedTest) {
-    //console.log(firstName);
-    let firstName;
-    const yearOfBirth = 1990;
-    if (passedTest) {
-        firstName = 'John';   
-    }
-        console.log(firstName + ', born in ' + yearOfBirth + ' is now officialyy alowed to drive a car.');
-}
-
-
-driversLicence6(true);
+// function driversLicence6(passedTest) {
+//     //console.log(firstName);
+//     let firstName;
+//     const yearOfBirth = 1990;
+//     if (passedTest) {
+//         firstName = 'John';   
+//     }
+//         console.log(firstName + ', born in ' + yearOfBirth + ' is now officialyy alowed to drive a car.');
+// }
 
 
-
+// driversLicence6(true);
 
 
 
@@ -52,78 +49,256 @@ driversLicence6(true);
 
 
 
-// Block Scoping Example
-
-let i = 23;
-
-for(let i = 0; i < 5; i++) {
-    console.log(i);
-}
-
-console.log(i);
 
 
 
-////////////////////////////////////
-// Lecture: Blocks and IIFs
+// // Block Scoping Example
 
-{
-    const a = 1;
-    let b = 2;
-    //var c =3;
-}
+// let i = 23;
 
-//console.log(a + b);
-//console.log(c);
+// for(let i = 0; i < 5; i++) {
+//     console.log(i);
+// }
+
+// console.log(i);
 
 
+
+// ////////////////////////////////////
+// // Lecture: Blocks and IIFs
+
+// {
+//     const a = 1;
+//     let b = 2;
+//     //var c =3;
+// }
+
+// //console.log(a + b);
+// //console.log(c);
+
+
+
+// //ES5
+
+// (function() {
+//     var c = 3;
+// })();
+
+// //console.log(c);
+
+
+
+// //////////////////////////////
+// // Lecture: Strings
+
+// // let firstName = 'John';
+// // let lastName = 'Smith';
+// // const yearOfBirth = 1990;
+
+// // function calcAge(year) {
+// //     return 2016 - year;
+// // }
+
+// // ES5
+
+// // console.log('This is ' + firstName + ' ' + lastName + '. He was born in ' + yearOfBirth + '. Today, he is '+ calcAge(yearOfBirth) + ' years old.');
+
+// // // ES6
+
+// // console.log(`This is ${firstName} ${lastName}. He was born in ${yearOfBirth}. Today, he is ${calcAge(yearOfBirth)} years old.`);
+
+// // const n = `${firstName} ${lastName}`;
+
+// // //ES6 String functions
+// // console.log(n.startsWith('J'));
+// // console.log(n.endsWith('h'));
+// // console.log(n.includes(' '));
+// // console.log(firstName.repeat(5));
+// // console.log(`${lastName}`.repeat(5));
+
+
+
+
+// ///////////////////////////////////
+// // Lecture: Arrow Functions
+
+// const years = [1990, 1965, 1982, 1937];
+
+// // ES5
+// var ages5 = years.map(function(el) {
+//     return 2016 - el;
+// });
+// console.log(ages5);
+
+// // ES6
+// let ages6 = years.map(el => 2016 - el);
+// console.log(ages6);
+
+// ages6 = years.map((el,index) => `Age element ${index +1}: ${2016 - el}.`);
+
+// console.log(ages6);
+
+// ages6 = years.map((el, index) => {
+//     const now = new Date().getFullYear();
+//     const age = now - el;
+//     return `Age element ${index + 1}: ${age}.`
+// });
+// console.log(ages6);
+
+
+// ///////////////////////////////////////
+// // Lecture: Arrow Functions 2
+
+// //ES5
+// var box5 = {
+//     color: 'green',
+//     position: 1,
+//     clickMe: function() {
+//         var self = this;
+//         document.querySelector('.green').addEventListener('click', function() {
+//             var str = 'This is box number ' + self.position + ' and it is ' + self.color;
+//             alert(str);
+//         })
+//     }
+// }
+
+// box5.clickMe();
+
+
+
+// //ES6
+// var box6 = {
+//     color: 'green',
+//     position: 1,
+//     clickMe: function() {
+//         //var self = this;
+//         document.querySelector('.green').addEventListener('click', () => {
+//             const str = 'This is box number ' + this.position + ' and it is ' + this.color;
+//             alert(str);
+//         })
+//     }
+// }
+
+// box6.clickMe();
+// //ES6
+// // var box66 = {
+// //     color: 'green',
+// //     position: 1,
+// //     clickMe: () => {
+// //         //var self = this;
+// //         document.querySelector('.green').addEventListener('click', () => {
+// //             const str = 'This is box number ' + this.position + ' and it is ' + this.color;
+// //             alert(str);
+// //         })
+// //     }
+// // }
+
+// // box66.clickMe();
+
+
+// function Person(name) {
+//     this.name = name;
+// }
+
+// //ES5
+// Person.prototype.myFriends5 = function(friends) {
+//     var arr = friends.map(function(el) {
+//         return this.name + ' is friends with ' + el;
+//     }.bind(this));
+//     console.log(arr);
+// }
+
+// var friends = ['Bob', 'Jane', 'Mark'];
+// new Person('John').myFriends5(friends);
+
+
+
+// //ES6
+// Person.prototype.myFriends5 = function(friends) {
+//     var arr = friends.map(el => `${this.name} is friends with + ${this.el}`);
+//     console.log(arr);
+// }
+
+// var friends = ['Bob', 'Jane', 'Mark'];
+// new Person('John').myFriends5(friends);
+// new Person('Mike').myFriends5(friends);
+
+
+
+
+// ////////////////////////////////////////
+// //// Lecture: Destructuring
+
+// // ES5
+// var john = ['John', 26];
+// //var name = john[0];
+// //var age = john[1];
+
+
+// // ES6
+// const [name, age] = ['John', 26];
+
+// console.log(name);
+// console.log(age);
+
+// const obj = {
+//     firstName: 'John',
+//     lastName: 'Smith'
+// };
+
+// const {firstName, lastName} = obj;
+
+// console.log(firstName);
+// console.log(lastName);
+
+// const {firstName: a, lastName: b} = obj;
+// console.log(a);
+// console.log(b);
+// function calcAgeRetirment(year) {
+//     const age = new Date().getFullYear() - year;
+//     return [age, 65 - age];
+// }
+// const [age2, retirement] = calcAgeRetirment(1990);
+// console.log(age2);
+// console.log(retirement);
+
+///////////////////////////////////////////
+// Lecture: Arrays
+
+const boxes = document.querySelectorAll('.box');
 
 //ES5
 
-(function() {
-    var c = 3;
-})();
-
-//console.log(c);
-
+var boxesArr5 = Array.prototype.slice.call(boxes);
+boxesArr5.forEach(function(el) {
+    el.style.backgroundColor = 'dodgerblue';
+});
 
 
-//////////////////////////////
-// Lecture: Strings
+//ES6
 
-let firstName = 'John';
-let lastName = 'Smith';
-const yearOfBirth = 1990;
-
-function calcAge(year) {
-    return 2016 - year;
-}
+const boxesArr6 = Array.from(boxes);
+boxesArr6.forEach(element => {
+    element.style.backgroundColor = 'dodgerblue'
+});
 
 // ES5
+/*
+for(var i = 0; i < boxesArr5.length; i++) {
+    if(boxesArr5[i].className === 'box blue') {
+        continue;
+    }
 
-console.log('This is ' + firstName + ' ' + lastName + '. He was born in ' + yearOfBirth + '. Today, he is '+ calcAge(yearOfBirth) + ' years old.');
+    boxesArr5[i].textContent = 'I chenged to blue!';
+}
+*/
 
 // ES6
 
-console.log(`This is ${firstName} ${lastName}. He was born in ${yearOfBirth}. Today, he is ${calcAge(yearOfBirth)} years old.`);
+for (const cur of boxesArr6) {
+    if(cur.className.includes('blue')) {
+        continue;
+    }
+    cur.textContent = 'I chenged to blue!';
 
-const n = `${firstName} ${lastName}`;
-
-//ES6 String functions
-console.log(n.startsWith('J'));
-console.log(n.endsWith('h'));
-console.log(n.includes(' '));
-console.log(firstName.repeat(5));
-console.log(`${lastName}`.repeat(5));
-
-
-
-
-///////////////////////////////////
-// Lecture: Arrow Functions
-
-const years = [1990, 1965, 1982, 1937];
-
-// ES5
-var 
-
+}
